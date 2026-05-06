4b68e4263b98a3a51ebcb69075ecd450011dd58d
Emilio <coshe1111@gmail.com>
2026-05-06 13:33:55 -0600

    Added random colors for snake and food
    Used random seeds using sys.max and assign the colors from a list of 5
    possible options. Use of a while loop to keep of randomizing to avoid
    both of them being the  same color.

--- snake.py
+++ snake.py
@@ -1,6 +1,7 @@
 from random import randrange
 from turtle import *
-
+import random
+import sys
 from freegames import square, vector
 
 # Include vectors to manage the game elements
@@ -8,6 +9,15 @@ food = vector(0, 0)
 snake = [vector(10, 0)]
 aim = vector(0, -10)
 
+colors = ['pink', 'orange', 'purple', 'yellow', 'green']
+
+my_seed = random.randrange(sys.maxsize)
+random.seed(my_seed)
+snake_color = random.choice(colors)
+food_color = ''
+
+while(food_color == snake_color or food_color == ''):
+    food_color = random.choice(colors)
 
 def change(x, y):
     """Change snake direction."""
@@ -67,9 +77,9 @@ def move():
     clear()
 
     for body in snake:
-        square(body.x, body.y, 9, 'black')
+        square(body.x, body.y, 9, snake_color)
 
-    square(food.x, food.y, 9, 'green')
+    square(food.x, food.y, 9, food_color)
     update()
     ontimer(move, 100)
 
